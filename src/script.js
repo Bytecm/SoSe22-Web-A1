206832688a8dc03fd34b67f34afd0adfd815a3b1
graityr <89629439+graityr@users.noreply.github.com>
2022-01-18 19:28:55 +0100

    working form on main page

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
-function calculate() {
-  /*
+function mouseTest() {
   document.getElementById("fname").value = "";
   document.getElementById("lname").value = "";
   document.getElementById("birthday").value = "";
@@ -7,10 +6,15 @@ function calculate() {
   document.getElementById("color").value = "";
 
   var i = Math.random();
+  var resultDiv = document.getElementById('result');
+
+  resultDiv.innerHTML = "";
 
   if (i <= 0.3) {
-    i.insertAdjacentHTML("afterend", "<p>test</p>");
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Hauskatze_langhaar.jpg/640px-Hauskatze_langhaar.jpg alt=Katze width=250/> <p>Du bist eine Katze!</p>";
+  } else if (i <= 0.6) {
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Akita_inu.jpeg/640px-Akita_inu.jpeg alt=Hund width=250/> <p>Du bist ein Hund!</p>";
+  } else {
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Whale_shark_Georgia_aquarium.jpg/640px-Whale_shark_Georgia_aquarium.jpg alt=Katze width=250/> <p>Du bist ein Fisch!</p>";
   }
-  */
-  console.log("hello");
 }
