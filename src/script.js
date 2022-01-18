eabd827b231091e4f38d4e353de15cb4d511ceb7
Bytecm <80549807+bytecm@users.noreply.github.com>
2022-01-18 18:44:40 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function calculate() {
+  /*
+  document.getElementById("fname").value = "";
+  document.getElementById("lname").value = "";
+  document.getElementById("birthday").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("color").value = "";
+
+  var i = Math.random();
+
+  if (i <= 0.3) {
+    i.insertAdjacentHTML("afterend", "<p>test</p>");
+  }
+  */
+  console.log("hello");
+}
