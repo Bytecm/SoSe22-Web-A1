861993294e79f6bc3d44ff2bd4ac79fac88253d8
graityr <89629439+graityr@users.noreply.github.com>
2022-02-01 15:10:34 +0100

    changed test result picture size

--- src/script.js
+++ src/script.js
@@ -11,18 +11,18 @@ function mouseTest() {
   resultDiv.innerHTML = "";
 
   if (i <= 0.03) {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Hauskatze_langhaar.jpg/640px-Hauskatze_langhaar.jpg alt=Katze width=250/> <p>Du bist eine Katze!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Hauskatze_langhaar.jpg/640px-Hauskatze_langhaar.jpg alt=Katze width=350/> <p>Du bist eine Katze!</p>";
   } else if (i <= 0.06) {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Akita_inu.jpeg/640px-Akita_inu.jpeg alt=Hund width=250/> <p>Du bist ein Hund!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Akita_inu.jpeg/640px-Akita_inu.jpeg alt=Hund width=350/> <p>Du bist ein Hund!</p>";
   } else if (i <= 0.1) {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Whale_shark_Georgia_aquarium.jpg/640px-Whale_shark_Georgia_aquarium.jpg alt=Katze width=250/> <p>Du bist ein Fisch!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Whale_shark_Georgia_aquarium.jpg/640px-Whale_shark_Georgia_aquarium.jpg alt=Katze width=350/> <p>Du bist ein Fisch!</p>";
   } else if (i <= 0.3) {
-    resultDiv.innerHTML = "<br><img src=http://esciencenews.com/files/images/201406269525560.jpg alt=Elefantenspitzmaus width=250/> <p>Du bist eine Elefantenspitzmaus!</p>";
+    resultDiv.innerHTML = "<br><img src=http://esciencenews.com/files/images/201406269525560.jpg alt=Elefantenspitzmaus width=350/> <p>Du bist eine Elefantenspitzmaus!</p>";
   } else if (i <= 0.5) {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/c/ca/Arvicola_terrestris.jpg alt=Ostschermaus width=250/> <p>Du bist eine Ostschermaus!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/c/ca/Arvicola_terrestris.jpg alt=Ostschermaus width=350/> <p>Du bist eine Ostschermaus!</p>";
   } else if (i <= 0.7) {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Neomys_anomalus.jpg/600px-Neomys_anomalus.jpg alt=Sumpfspitzmaus width=250/> <p>Du bist eine Sumpfspitzmaus!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Neomys_anomalus.jpg/600px-Neomys_anomalus.jpg alt=Sumpfspitzmaus width=350/> <p>Du bist eine Sumpfspitzmaus!</p>";
   } else {
-    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/a/ab/Razer_Naga_2014_MMO_Gaming_Mouse_%2814714867599%29.jpg alt=Katze width=250/> <p>Du bist eine Computermaus!</p>";
+    resultDiv.innerHTML = "<br><img src=https://upload.wikimedia.org/wikipedia/commons/a/ab/Razer_Naga_2014_MMO_Gaming_Mouse_%2814714867599%29.jpg alt=Katze width=350/> <p>Du bist eine Computermaus!</p>";
   }
 }
